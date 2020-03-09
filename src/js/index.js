4fc24b61805f9e9fdba34874eabf073e57a180ae
Florin Ionescu <sii76354@adobe.com>
2020-03-09 16:36:17 +0200

    Search model implemented

--- src/js/index.js
+++ src/js/index.js
@@ -1,27 +1,6 @@
 // Global app controller
-import axios from "axios";
+import Search from "./models/Search";
 
-// FIRST EXAMPLE THAT WORKS
-// async function getResults(query) {
-//   const proxy = "https://cors-anywhere.herokuapp.com/";
-//   const key = "DASDAFGAGSAFASD";
-//   const res = await axios(
-//     `${proxy}http://food2fork.com/api/search?key=${key}&q=${query}`
-//   );
-//   console.log(res);
-// }
-
-async function getResults(query) {
-    const proxy = "https://cors-anywhere.herokuapp.com/";
-    try {
-      const res = await axios(
-        `${proxy}https://forkify-api.herokuapp.com/api/search?q=${query}`
-      );
-    const recipes = res.data.recipes;
-    console.log(recipes);
-  } catch (error){
-      alert(error)
-  }
-}
-
-getResults("pizza");
+const search = new Search("pizza");
+console.log(search);
+search.getResults();
---
+++ src/js/models/Search.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+
+export default class Search {
+  // in constructor will write the properties we want that class to have
+  constructor(query) {
+    this.query = query;
+  }
+  async getResults() {
+    const proxy = "https://cors-anywhere.herokuapp.com/";
+    try {
+      const res = await axios(
+        `${proxy}https://forkify-api.herokuapp.com/api/search?q=${this.query}`
+      );
+      // store the data into the Search object in a property custom called result
+      this.result = res.data.recipes;
+      console.log(this);
+      // console.log(this.result);
+    } catch (error) {
+      alert(error);
+    }
+  }
+}
+
+// FIRST EXAMPLE THAT WORKS
+// async function getResults(query) {
+//   const proxy = "https://cors-anywhere.herokuapp.com/";
+//   const key = "DASDAFGAGSAFASD";
+//   const res = await axios(
+//     `${proxy}http://food2fork.com/api/search?key=${key}&q=${query}`
+//   );
+//   console.log(res);
+// }
